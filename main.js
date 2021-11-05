2b198535dee1add84fe2d04df00a078a674fc5e2
Palme412 <jakepalmer412@gmail.com>
2021-11-05 16:25:32 -0500

    hyperlinks and button fix

--- main.js
+++ main.js
@@ -6,4 +6,7 @@ buttonElement = document.querySelector(".buttonBig");
 buttonElement.onclick = function(e) {
     let titleName = document.querySelector(".titleName");
     titleName.style.color = "red";
+    let buttonInnerText = document.querySelector("#button");
+    buttonInnerText.innerHTML = "Emailed!";
 }
+
